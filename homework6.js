2f535df22e7bd890463550fe96c365dc8dadf8cf
Svitlana Yatsenko <svetlana.kovaleva@md.p2h.com>
2025-01-16 11:05:30 +0100

    files

--- homework6.js
+++
@@ -1,5 +0,0 @@
-const text = "Wonderful Joyful Happiness Time Task Apple gggggshhshshsaa AAAAAAAAAA aaaaaaaaa"
-const pattern = /\b[^aA\s]{6,}\b/g;
-const matches = text.match(pattern);
-
-console.log(matches);
